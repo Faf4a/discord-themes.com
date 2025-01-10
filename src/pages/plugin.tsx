a1c577ad056f967c4ae62d3e4ec122389413dbf4
Fafa <87046111+Faf4a@users.noreply.github.com>
2025-01-10 12:58:14 +0000

    lint

--- src/pages/plugin.tsx
+++ src/pages/plugin.tsx
@@ -1,4 +1,4 @@
-import { Download, ExternalLink, Github, Terminal, Heart, Star } from "lucide-react";
+import { ExternalLink, Heart, Star } from "lucide-react";
 import { Button } from "@components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
 import Head from "next/head";
