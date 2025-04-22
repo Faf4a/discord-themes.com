ba699978113ca785df4f939140bdae9e294e320e
Fafa <87046111+Faf4a@users.noreply.github.com>
2025-04-22 15:30:31 +0200

    fix: site crash

--- src/pages/theme/[id].tsx
+++ src/pages/theme/[id].tsx
@@ -1,38 +1,69 @@
 "use client";
 
 import { useRouter } from "next/router";
-import type { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import { useEffect } from "react";
+import type {
+	GetStaticPaths,
+	GetStaticProps,
+	InferGetStaticPropsType
+} from "next";
 import App from "@components/page/theme-info";
 import { type Theme } from "@types";
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const res = await fetch("https://raw.githubusercontent.com/Faf4a/stunning-spoon/refs/heads/main/themes.json");
-    const themes = await res.json();
+	const res = await fetch(
+		"https://raw.githubusercontent.com/Faf4a/stunning-spoon/refs/heads/main/themes.json"
+	);
+	const themes = await res.json();
 
-    const paths = themes.map((theme: Theme) => ({
-        params: { id: String(theme.id) }
-    }));
+	const paths = themes.map((theme: Theme) => ({
+		params: { id: String(theme.id) }
+	}));
 
-    return {
-        paths,
-        fallback: "blocking"
-    };
+	return {
+		paths,
+		fallback: "blocking"
+	};
 };
 
-// eslint-disable-next-line no-unused-vars
-export const getStaticProps = (async (context) => {
-    const res = await fetch("https://raw.githubusercontent.com/Faf4a/stunning-spoon/refs/heads/main/themes.json");
-    const themes = await res.json();
-    return { props: { themes }, revalidate: 60 };
+export const getStaticProps = (async context => {
+	const res = await fetch(
+		"https://raw.githubusercontent.com/Faf4a/stunning-spoon/refs/heads/main/themes.json"
+	);
+	const themes = await res.json();
+	return { props: { themes }, revalidate: 60 };
 }) satisfies GetStaticProps<{
-    themes: Theme[];
+	themes: Theme[];
 }>;
 
-export default function ThemePage({ themes }: InferGetStaticPropsType<typeof getStaticProps>) {
-    const router = useRouter();
-    const { id } = router.query;
+export default function ThemePage({
+	themes
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+	const router = useRouter();
+	const { id } = router.query;
 
-    const theme = themes.find((x) => x.id == id);
+	let theme = themes.find(x => x.id == id);
 
-    return <App id={id as string} theme={theme} />;
+	// try to get the theme name instead
+	const validTheme =
+		!theme && typeof id === "string"
+			? themes.find(x => x.name.toLowerCase() === id.toLowerCase())
+			: null;
+
+	useEffect(() => {
+		if (!id) return;
+
+		if (validTheme) {
+			router.replace(`/${validTheme.id}`);
+		} else if (!theme) {
+			router.replace("/");
+		}
+	}, [id, theme, themeByName, router]);
+
+  // prevent site crashing during redirect
+	if (!theme) {
+		return null;
+	}
+
+	return <App id={id as string} theme={theme} />;
 }
